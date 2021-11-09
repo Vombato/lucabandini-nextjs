a47ddc572f6849a8140abcd48b9a575bdec40190
Luca Bandini <luca.vombato@gmail.com>
2021-11-09 19:00:39 +0100

    Fixed references

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Luca Bandini',
+    site_url: 'https://lucabandini.it',
+    feed_url: 'https://lucabandini.it/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
