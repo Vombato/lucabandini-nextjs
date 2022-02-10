e099f61f0145de7ee21b86e74629643c6a053af1
Luca Bandini <luca.vombato@gmail.com>
2022-02-10 01:14:37 +0100

    Test new changes

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Luca Bandini - Website',
+      description: 'Website, portfolio and Social Links',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
@@ -24,10 +24,9 @@ class MyDocument extends Document {
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
+          <meta name="twitter:site" content="@VperVombato" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
